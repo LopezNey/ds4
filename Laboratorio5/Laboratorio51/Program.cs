2975c585e31cc204c4cb4271c050983b4385db48
neish <neish@MARTINSLOVER>
2025-09-24 22:20:34 -0500

    Laboratorio 5 completo

---
+++ Laboratorio5/Laboratorio51/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private int[] sueldos;
+
+    public void Cargar()
+    {
+        sueldos = new int[6];
+        for (int f = 1; f <= 5; f++)
+        {
+            Console.Write("Ingrese sueldo del operario " + f + ": ");
+            sueldos[f] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    public void Imprimir()
+    {
+        Console.Write("Los 5 sueldos de los operarios \n");
+        for (int f = 1; f <= 5; f++)
+        {
+            Console.Write("["+sueldos[f]+"] ");
+        }
+        Console.ReadKey();
+
+    }
+    private static void Main(string[] args)
+    {
+        Program pv = new Program();
+        pv.Cargar();
+        pv.Imprimir();
+    }
+} 
---
+++ Laboratorio5/Laboratorio52/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private int[,] mat;
+
+    public void Ingresar()
+    {
+        mat = new int[3, 4];
+        for (int f = 0; f < 3; f++)
+        {
+            for (int c = 0; c < 4; c++)
+            {
+                Console.Write("Ingrese posicion ["+(f+1)+","+(c+1)+"]: ");
+                mat[f, c] = Convert.ToInt32(Console.ReadLine());
+            }
+
+        }
+    }
+
+    public void Imprimir()
+    {
+        for (int f = 0; f < 3; f++)
+        {
+            for (int c = 0;c < 4; c++)
+            {
+                Console.Write(mat[f, c] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+
+    }
+    private static void Main(string[] args)
+    {
+        Program pv = new Program();
+        pv.Ingresar();
+        pv.Imprimir();
+    }
+}
